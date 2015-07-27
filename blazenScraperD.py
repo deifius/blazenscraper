fbe7623ab1185fb09a22bd6c3105ead23862a45e
deifius <sunji.roaoul@gmail.com>
2015-07-27 11:02:18 -0500

    first commit

---
+++ blazenScraperD.py
@@ -0,0 +1,35 @@
+ls#!/usr/bin/env python
+
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
+from time import sleep
+import re
+from twilio.rest import TwilioRestClient 
+
+# Twilio Authorizations	
+ACCOUNT_SID = "" 
+AUTH_TOKEN = "" 
+#BlazenGems location
+site = "http://blazengems.com/store/index.php?main_page=products_new&zenid=vub07oc2b512ktnu1ol7nmei35"
+yourcellphone = ""
+originnumber = ""
+
+while 1==1:
+	html = urlopen(site).read()
+	datesadded = re.compile("Date Added: \w+ \w+ \w+, \w+").findall(html)
+	lastdates = open('lastdateset.txt').read().split("\n")
+	if "".join(datesadded) != "".join(lastdates):
+		client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+		client.messages.create(
+			to = yourcellphone, 
+			from_ = originnumber, 
+			body="Hey amigo! http://blazengems.com/store/index.php?main_page=products_new",)
+		#print datesadded 
+		#print "\n\n\n"
+		#print lastdates
+
+	newdates = open('lastdateset.txt','w')
+	for date in datesadded:
+		newdates.write(date +"\n")
+	newdates.close()
+	sleep(900)
---
+++ readme.md
@@ -0,0 +1,3 @@
+welcome to the blazenscraper.  My amigo has begun an American gem collection. To get the drop on recently unearthed Missouri river sapphires, I wrote him this little script that sits on his home server.  Whenever blazen gems (or other gem hunters, mod to suit) post new finds online, the server will notify said amigo via text.
+
+Gem collectors can get first pick of all American gems without having to slavishly check web sites.  Sweet deal!  I got a custom bike out of it.
